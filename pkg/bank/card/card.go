cd714b3849f5cd85e930b1736ff8a48f24aef439
GitUsernameAfk <69089755+GitUsernameAfk@users.noreply.github.com>
2020-09-20 22:44:08 +0500

    Исправлены ошибки

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -1,14 +1,15 @@
 package payment
-import ( 
+
+import (
 	"bank/pkg/github/types"
 )
 
 //PaymntS uses for sources from cards
 func PaymntS(cards types.Card) types.PaymntSource {
 	var PaymntSourc types.PaymntSource
-	if(cards.Active && cards.Balance>0) {
+	if cards.Active && cards.Balance > 0 {
 		PaymntSourc.Number = cards.PAN
-		PaymntSourc.Balance=cards.Balance
+		PaymntSourc.Balance = cards.Balance
 		PaymntSourc.Type = cards.Name
 	}
 	return PaymntSourc
