b5898848754fd024afc0901ed5df6e54e0ec86cf
GitUsernameAfk <69089755+GitUsernameAfk@users.noreply.github.com>
2020-09-20 21:18:27 +0500

    добавлены типы и конфингурация

---
+++ pkg/github/types/types.go
@@ -0,0 +1,42 @@
+package types
+
+//Money is type for money (in lil units)
+type Money int64
+
+//Currency is type for currencies(TJS,USD,RUS etc.) 
+type Currency string
+
+//codes for currencies
+const (
+	TJS Currency = "TJS"
+	USD Currency = "USD"
+	RUS Currency = "RUS"
+)
+
+//PAN is type for PAN of debet cards  
+type PAN string
+
+//Card is type for info of card
+type Card struct{
+	ID				int
+	PAN				PAN
+	Color			string
+	Name			string
+	Balance			Money		
+	Currency		Currency
+	Active			bool
+
+}
+
+//Payment uses for payments of cards
+type Payment struct {
+	ID		int64
+	Amount  Money
+}
+
+//PaymentSource is struct type fot sources from card 
+type PaymentSource struct {
+	Type 		string
+	Number 		string
+	Balance		Money
+}
